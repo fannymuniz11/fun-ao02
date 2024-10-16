cd78117909b29c9c64f761fe3a9c905f1d9fbb5b
fannymuniz11 <munizfanny03@gmail.com>
2024-10-16 13:47:01 -0300

    funçao02

---
+++ funçao02.py
@@ -0,0 +1,4 @@
+# Crie uma função chamada verificar_paridade que receba um número inteiro como argumento e retorne uma mensagem indicando se o número é par ou ímpar.
+
+def verificar_paridade(numero):
+    return "O número é par." if numero % 2 == 0 else "O número é ímpar."
